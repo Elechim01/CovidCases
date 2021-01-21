dbc967c9ccfe0c419eb5feab85f25dc80ce755a5
Elechim01 <mikymanny015@gmail.com>
2021-01-21 22:51:23 +0100

    Initial Commit

---
+++ CovidCases/CovidCasesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidCasesApp.swift
+//  CovidCases
+//
+//  Created by Michele on 21/01/21.
+//
+
+import SwiftUI
+
+@main
+struct CovidCasesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
